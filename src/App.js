3406955fb4d2c5ffbc9ded6cde6c28c564a09dd7
alvinjamal <alvinjamalazkya@gmail.com>
2022-12-25 13:05:10 +0700

    ad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import Router from "./Config/router";
-import NavbarComponent from "../src/Components/base/header";
 
 function App() {
   return (
     <div className="App">
-      <NavbarComponent />
       <Router />
     </div>
   );
